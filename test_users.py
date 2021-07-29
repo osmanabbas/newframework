8994840cce01f26d1958e9b2aed700a87bdb6325
osmanabbas <87112794+osmanabbas@users.noreply.github.com>
2021-07-29 12:48:29 -0400

    tests

---
+++ test_users.py
@@ -0,0 +1,26 @@
+import unittest, sys, os
+
+sys.path.append('../framework')
+from demo import app, db
+
+class UsersTests(unittest.TestCase):
+  def setUp(self):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    self.app = app.test_client()
+    db.drop_all()
+    db.create_all()
+    
+  def register(self, username, email, password):
+    return self.app.post('/register',
+                         data=dict(username=username,
+                                   email=email,
+                                   password=password,
+                                   confirm_password=password),
+                         follow_redirects=True)
+
+  def test_valid_user_registration(self):
+      response = self.register('test', 'test@example.com', 'FlaskIsAwesome')
+      self.assertEqual(response.status_code,200)
+
+if __name__ == "__main__":
+  unittest.main()
